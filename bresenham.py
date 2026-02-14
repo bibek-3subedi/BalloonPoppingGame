b87fd588b8f2e4b2f0ebb0c242b95a202fd7f433
bibek_3subedi <bibek.3subedi@gmail.com>
2026-02-15 00:54:26 +0545

    Add bresenham.py file

---
+++ bresenham.py
@@ -0,0 +1,31 @@
+import math
+def bresenham(x1, y1, x2, y2):
+    points = []
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    x, y = x1, y1
+
+    sx = 1 if x2 > x1 else -1
+    sy = 1 if y2 > y1 else -1
+
+    if dx > dy:
+        err = dx / 2
+        while x != x2:
+            points.append((x, y))
+            err -= dy
+            if err < 0:
+                y += sy
+                err += dx
+            x += sx
+    else:
+        err = dy / 2
+        while y != y2:
+            points.append((x, y))
+            err -= dx
+            if err < 0:
+                x += sx
+                err += dy
+            y += sy
+
+    points.append((x2, y2))
+    return points
